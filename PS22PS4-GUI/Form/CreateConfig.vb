20d1bc231608d139884ebf18502e409187f4ef16
Nazky <56560636+Nazky@users.noreply.github.com>
2022-10-23 16:04:43 +0200

    update (fix)

--- PS22PS4-GUI/Form/CreateConfig.vb
+++ PS22PS4-GUI/Form/CreateConfig.vb
@@ -4,7 +4,7 @@ Imports System.Windows.Forms.VisualStyles.VisualStyleElement
 Public Class CreateConfig
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Try
-            System.IO.File.WriteAllText("bin\configs\" & My.Settings.GID & ".conf", TextBox1.Text)
+            System.IO.File.WriteAllText("bin\configs\" & My.Settings.GID & ".conf", RichTextBox1.Text)
             MsgBox("LUA saved to" & vbCrLf & Application.StartupPath & "bin\configs\" & My.Settings.GID & ".conf", MsgBoxStyle.Information, "PS22PS4-GUI")
         Catch ex As Exception
             MsgBox(ex.Message, MsgBoxStyle.Critical, "PS22PS4-GUI")
